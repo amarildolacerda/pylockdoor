9c3b42bc8363af335d23b06b6755eafbb56b1ccc
Amarildo de Lacerda <amarildo_51@msn.com>
2023-02-15 16:46:08 -0300

    + /pin

--- lampada/src/portal.cpp
+++ lampada/src/portal.cpp
@@ -36,9 +36,13 @@ void Portal::reset()
     delay(1000);
 }
 
-String button(String name, String link, String style = "")
+String button(const String name, const char *link, const char *style = "", const String hidden = "")
 {
-    return stringf("<br/><form action='%s' method='get'><button %s>%s</button></form>", link, style == "" ? "" : stringf("class='%s'", style), name);
+    return stringf("<br/><form action='%s' method='get'> %s <button %s>%s</button></form>", link, hidden, (style == "") ? "" : String(stringf("class='%s'", style)), name);
+}
+String input(const String name, const String value)
+{
+    return String(stringf("<input type=\"text\" name=\"%s\" value=\"%s\">", name, value));
 }
 
 void setManager(WiFiManager *wf)
@@ -53,11 +57,40 @@ void Portal::setupServer()
                {
         WiFiManager wf  ;
         setManager(&wf);
-    
-        String pg = button("GPIO", "/gs");
-        pg += button("Recarregar","/");
+        String pg = "";
+        JsonObject mode = homeware.getMode();
+        for (JsonPair k : mode)
+        {   
+          if (k.value().as<String>()=="in"){
+              String p1 = k.key().c_str();
+              String v1 = k.value().as<String>();
+              int v = homeware.readPin(p1.toInt(), v1);
+              String s = (v == 1) ? "ON" : (v > 0) ? String(v)
+                                                   : "OFF";
+              String hd =input("p", p1);
+              hd += input("q", ((s == "ON") ? "OFF" : "ON"));
+              Serial.println(hd);
+
+              pg += button(s, "/pin", "D",hd );
+          }
+        }
+
+        pg += button("GPIO", "/gs");
         portal.server->send(200, "text/html", wf.pageMake("Homeware", pg)); });
 
+    server->on("/pin", []()
+               {
+                //if (portal.server->hasArg("p") && portal.server->hasArg("q")){
+                String p = portal.server->arg("p");
+                String q = portal.server->arg("q");
+                Serial.println(p);
+                Serial.println(q);
+                //if (p && t){
+                    homeware.writePin(p.toInt(),(q=="ON")?1:0);
+                //}
+                //}
+                portal.server->sendHeader("Location", String("/"), true);
+                portal.server->send(302, "text/plain", ""); });
     server->on("/gs", []()
                {
         WiFiManager wf ;
@@ -67,12 +100,13 @@ void Portal::setupServer()
         pg+="<table style=\"width:80%\">";
         pg+="<thead><tr><th>Pin</th><th>Mode</th><th>Value</th></tr></thead><tbody>";
         JsonObject mode = homeware.getMode();
-        //JsonObject values = homeware.getValues();
         for (JsonPair k : mode)
         {
-            int v = homeware.readPin( int(k.key().c_str())  , k.value().as<String>());
+            String p1 = k.key().c_str();
+            String v1 = k.value().as<String>();
+            int v = homeware.readPin(p1.toInt(), v1);
             String s = (v==1)?"ON": (v>0)?  String(v):"OFF";
-            pg += stringf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", k.key().c_str(), k.value().as<String>(),s);
+            pg += stringf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", p1, v1,s);
         }
 
         pg+="</tbody></table>";
