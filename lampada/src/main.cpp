8d96b36f6570351a2c20a4039c9001012c852bcb
Amarildo de Lacerda <amarildo_51@msn.com>
2023-03-15 21:34:16 -0300

    + gaz define

--- lampada/src/main.cpp
+++ lampada/src/main.cpp
@@ -19,6 +19,8 @@ Protocol prot;
 
 #define BAUD_RATE 115200 // Change baudrate to your need
 
+//#define GAZ
+
 void defaultConfig()
 {
 #ifdef SONOFF_BASIC
@@ -26,14 +28,23 @@ void defaultConfig()
   homeware.doCommand("gpio 14 mode in");
   homeware.doCommand("gpio 14 trigger 12 bistable");
 #endif
-  // homeware.doCommand("reset factory");
-  // homeware.doCommand("save");
-  // homeware.doCommand("reset");
-  // homeware.doCommand(stringf("gpio %d mode in", BUTTON_PIN));
-  // homeware.doCommand(stringf("gpio %d mode out", RELAY_PIN));
-  // homeware.doCommand("gpio 4 trigger 15 monostable");
-  // homeware.doCommand("gpio 15 device onoff");
-  // homeware.printConfig();
+// homeware.doCommand("reset factory");
+// homeware.doCommand("save");
+// homeware.doCommand("reset");
+// homeware.doCommand(stringf("gpio %d mode in", BUTTON_PIN));
+// homeware.doCommand(stringf("gpio %d mode out", RELAY_PIN));
+// homeware.doCommand("gpio 4 trigger 15 monostable");
+// homeware.doCommand("gpio 15 device onoff");
+// homeware.printConfig();
+#ifdef GAZ
+  homeware.doCommand("gpio A0 mode adc");
+  homeware.setKey("adc_min", "0");
+  homeware.setKeyIfNull("adc_max", "240");
+  homeware.doCommand("gpio 15 mode out");
+  homeware.doCommand("gpio A0 trigger 15 monostable");
+  homeware.doCommand("gpio 15 device onoff");
+  homeware.doCommand("show config");
+#endif
 }
 
 void setupServer()
@@ -56,6 +67,8 @@ void setup()
   prot.setup();
 #else
   Serial.printf("\r\n\r\n");
+  homeware.prepare();
+  defaultConfig();
   homeware.setup(&server);
 #endif
 
@@ -66,7 +79,6 @@ void setup()
 #endif
 
   setupServer();
-  defaultConfig();
 }
 
 void loop()
