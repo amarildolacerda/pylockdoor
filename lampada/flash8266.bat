28f2d6c2b8e15c0942f91fd4d06a306be2ac3267
Amarildo de Lacerda <amarildo_51@msn.com>
2023-03-17 00:04:19 -0300

    + DDEBUG_ON/OFF

--- lampada/flash8266.bat
+++ lampada/flash8266.bat
@@ -1,1 +1,7 @@
 esptool --port %1 --baud 1000000 write_flash  -fm dio --flash_size=detect 0 .pio/build/d1/firmware.bin
+
+rem arduino clound aiot
+rem deviceid - gaz
+rem ecf409e7-ec4d-4fb1-ba93-1f898c7484fd
+rem secret_key
+rem 06JIUEBMQFFUAMT0SYKN
